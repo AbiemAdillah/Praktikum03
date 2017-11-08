58ec2267678eed56c538ad2843444499559c6da6
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 17:03:37 +0800

    membuat program menghitung berat dan tinggi badan

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int tb,bb,hasil1,hasil2;
+	cout<<"Menghitung Badan Ideal"<<endl;
+	cout<<"----------------------------"<<endl;
+	cout<<"Masukkan Tiggi Badan = ";cin>>tb;
+	cout<<"Masukkan Berat Badan = ";cin>>bb;
+
+}
