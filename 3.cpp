e8bffbf5f27466c65c5dcf97f78acc1fd1d91bb1
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:35:45 +0800

    membuat program hitung Gaji

---
+++ 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double jk,lb,jl,jt;
+	long gt,gb;
+	
+	cout<<"Program Hitung Gaji"<<endl;
+	cout<<"---------------------------"<<endl;
+	cout<<"Lama Jam Kerja Perminggu = ";cin>>jk;
+	cout<<"---------------------------"<<endl;
