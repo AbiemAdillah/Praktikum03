8ceb1fdc55e768b23b9cce2f3e36d32ff9d76ba3
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:22:14 +0800

    mencari selisih absolute dari nilai x dan y

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+		int X,Y,Z;
+		cout <<"Masukkan Nilai X\t = "; cin>>X;
+		cout <<"Masukkan Nilai Y\t = "; cin>>y;
+		
+		if(X<Y){
+			Z=Y-X;
+}
+		else{
+			Z=X-Y;
+		}
+		cout<<"Hasil Pengurangan \t = "<<Z;
+}
