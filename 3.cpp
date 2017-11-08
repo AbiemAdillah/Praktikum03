b4cf27adb45b754b635ac1748af08e80bfa16b12
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:37:39 +0800

    potongan 15% pajak untuk semua karyawan

--- 3.cpp
+++ 3.cpp
@@ -9,3 +9,14 @@ int main(){
 	cout<<"---------------------------"<<endl;
 	cout<<"Lama Jam Kerja Perminggu = ";cin>>jk;
 	cout<<"---------------------------"<<endl;
+	
+		if (jk>40){
+		cout<<"Gaji Pokok \t = Rp 2.000.000"<<endl;
+		cout<<"Jam Kerja Standart \t = 40 Jam"<<endl;
+			jl=lb*1.5;
+			jt=40+jl;
+		cout<<"Total Jam Kerja\t = "<<"Rp "<<gt<<endl;
+		cout<<"Potongan \t = 15%"<<endl;
+			gb=gt-(0.15*gt);
+	}
+	
