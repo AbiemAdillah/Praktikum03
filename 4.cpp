d356cc4b7efda4e2120de6f3a4b2d4f8afe8d78a
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:53:31 +0800

    membuat program menentukan nilai positif negatif dan nol

---
+++ 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+	int x;
+	cout<<"POSITIF, NEGATIF, NOL"<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"Masukkan Angka  =   ";
+	cin>>x;
+	cout<<"---------------------"<<endl;
+
+}
