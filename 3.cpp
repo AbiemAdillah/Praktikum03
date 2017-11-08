f27b3dc7787cc32d83f89fd18fb10edac2007e9b
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:48:11 +0800

    perbaikan

--- 3.cpp
+++ 3.cpp
@@ -11,13 +11,17 @@ int main(){
 	cout<<"---------------------------"<<endl;
 	
 		if (jk>40){
-		cout<<"Gaji Pokok \t = Rp 2.000.000"<<endl;
+		cout<<"Gaji Pokok \t = Rp 2000.000"<<endl;
 		cout<<"Jam Kerja Standart \t = 40 Jam"<<endl;
+		lb=jk-40
+		cout<<"Lembur\t\t\t = "<<lb<<" Jam"<<endl;
 			jl=lb*1.5;
 			jt=40+jl;
-		cout<<"Total Jam Kerja\t = "<<"Rp "<<gt<<endl;
+		cout<<"Total Jam Kerja\t = "<<jt<<"Jam"<<endl;
+		gt=2000000+(jl*100000);
 		cout<<"Potongan \t = 15%"<<endl;
 			gb=gt-(0.15*gt);
+			cout<<"Gaji Bersih\t\t = "<<"Rp "<<gb;
 	}
 	else if (jk<=40){
 		cout<<"Gaji Pokok\t = Rp 2.000.000"<<endl;
