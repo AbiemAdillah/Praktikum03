236d03d452b8ddfb3927d92b6278e4728650ad28
AbiemAdillah <Abiem.adillah@gmail.com>
2017-11-08 16:40:14 +0800

    menghitung bonus dari lembur

--- 3.cpp
+++ 3.cpp
@@ -19,4 +19,19 @@ int main(){
 		cout<<"Potongan \t = 15%"<<endl;
 			gb=gt-(0.15*gt);
 	}
+	else if (jk<=40){
+		cout<<"Gaji Pokok\t = Rp 2.000.000"<<endl;
+		cout<<"Jam Kerja Standart \t = 40 Jam"<<endl;
+			lb=0;
+		cout<<"Lembur\t\t = "<<lb<<" Jam"<<endl;
+			jl=lb*1.5;
+			jt=40+jl;
+		cout<<"Total Jam Kerja\t = "<<jt<<" Jam"<<endl;
+			gt=2000000+(jl*100000);
+		cout<<"Total Gaji\t = "<<"Rp "<<gt<<endl;
+			gb=gt-(0.15*gt);
+		cout<<"Gaji Bersih\t = "<<"Rp "<<gb;	
+	}
+return 0;
+}
 	
